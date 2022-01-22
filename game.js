0cb48bdd81bc023595a773ba104fae6068b7a42a
Chetonix <chetonix@gmail.com>
2022-01-22 17:31:06 +0530

    add in part sol to simon game

--- game.js
+++ game.js
@@ -1,45 +1,20 @@
-idList = ["green", "red", "yellow", "blue"];
-randomID = idList[newSequence()];
-gamePattern = [];
-gamePattern.push(randomID);
-
-$("#" + randomID)
-  .fadeOut(100)
-  .fadeIn(100);
-
-var audio = new Audio("sounds/" + randomID + ".mp3");
-audio.play();
-
-$(document).on("keydown", function (event) {
-  gameOver = false;
-  while (!gameOver) {
-    soundList = ["green.mp3", "red.mp3", "yellow.mp3", "blue.mp3", "wrong.mp3"];
-
-    $("#" + idList[randNumber]).addClass("pressed");
-
-    // $("#green").on("click", function (event) {
-    //   console.log(event);
-    // });
-
-    // switch (randNumber) {
-    //   case 1:
-
-    //     break;
-    //   case 1:
-    //     break;
-    //   case 1:
-    //     break;
-    //   case 1:
-    //     break;
-    //   default:
-    //     break;
-    // }
-
-    gameOver = true;
-  }
-});
+var idList = ["green", "red", "yellow", "blue"];
+var gamePattern = [];
 
 function newSequence() {
   var randNumber = Math.floor(Math.random() * 4);
-  return randNumber;
+
+  var randomID = idList[randNumber];
+
+  gamePattern.push(randomID);
+
+  $("#" + randomID)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  let audio = new Audio("sounds/green.mp3");
+  audio.play();
 }
+$(window).load(function () {
+  newSequence();
+});
