7516e3809bba83625488842f5cd7306d06a90eb2
Chetonix <chetonix@gmail.com>
2022-01-23 16:02:04 +0530

    add some solution to the game Simon Game

--- game.js
+++ game.js
@@ -1,7 +1,13 @@
 var idList = ["green", "red", "yellow", "blue"];
 var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+started = false;
 
 function newSequence() {
+  userClickedPattern = [];
+  
+  $("#level-title").text("Level " + level);
   var randNumber = Math.floor(Math.random() * 4);
 
   var randomID = idList[randNumber];
@@ -11,9 +17,45 @@ function newSequence() {
   $("#" + randomID)
     .fadeOut(100)
     .fadeIn(100);
+  playSound(randomID);
 
-  var audio = new Audio("sounds/green.mp3");
+  $(".btn").click(
+    checkAnswer(currentLevel);
+  );
+  // checkAnswer(level);
+  // level++;
+}
+
+$(".btn").click(function () {
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+  console.log(userClickedPattern);
+
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+});
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
-newSequence();
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+
+  setTimeout(() => {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+$(document).keypress(function () {
+  if (!started) {
+    started = true;
+    newSequence();
+}
+  setTimeout(newSequence(), 100);
+});
+
+function checkAnswer(currentLevel) {
+
+}
